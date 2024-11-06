4410e8630c9b460b37a0f02f7ba5b258bdb57077
adrian.borta <adrian.borta@accenture.com>
2024-11-06 19:02:55 +0100

    [feat] - add PnBeanConfig in inject templatePath

--- src/main/java/it/pagopa/pn/templates/engine/config/PnBeanConfig.java
+++ src/main/java/it/pagopa/pn/templates/engine/config/PnBeanConfig.java
@@ -1,7 +1,6 @@
 package it.pagopa.pn.templates.engine.config;
 
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,9 +9,6 @@ import org.springframework.ui.freemarker.FreeMarkerConfigurationFactoryBean;
 @Data
 @Configuration
 public class PnBeanConfig {
-
-    @Autowired
-    private PnTemplateConfig pnTemplateConfig;
 
     @Bean
     public FreeMarkerConfigurationFactoryBean freemarkerConfig(@Value("${templates.templatePath}") String templatePath) {
