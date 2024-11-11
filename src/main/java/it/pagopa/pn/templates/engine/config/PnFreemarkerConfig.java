5fb31d0712912d8bf83718c4b36f936f911b5084
adrian.borta <adrian.borta@accenture.com>
2024-11-11 17:46:22 +0100

    [feat] - correction PnFreemarkerConfig

--- src/main/java/it/pagopa/pn/templates/engine/config/PnFreemarkerConfig.java
+++ src/main/java/it/pagopa/pn/templates/engine/config/PnFreemarkerConfig.java
@@ -2,18 +2,20 @@ package it.pagopa.pn.templates.engine.config;
 
 import it.pagopa.pn.templates.engine.exceptions.ExceptionTypeEnum;
 import it.pagopa.pn.templates.engine.exceptions.PnGenericException;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
 @Configuration
+@AllArgsConstructor
 public class PnFreemarkerConfig {
+
+    private final freemarker.template.Configuration configuration;
 
     @Bean
     public freemarker.template.Configuration freemarkerConfig(@Value("${templatesPath}") String templatePath) {
         try {
-            freemarker.template.Configuration configuration = new freemarker.template.Configuration(freemarker.template.Configuration.VERSION_2_3_31);
             configuration.setClassLoaderForTemplateLoading(getClass().getClassLoader(), "/".concat(templatePath));
             configuration.setDefaultEncoding("UTF-8");
             configuration.setLogTemplateExceptions(false);
