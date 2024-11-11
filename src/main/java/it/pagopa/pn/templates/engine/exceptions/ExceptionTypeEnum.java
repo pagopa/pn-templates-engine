e8cf1cd4bd855832d2ba245bf1addc1034147c15
pasqualino-cristaudo-acn <pasqualino.cristaudo@accenture.com>
2024-11-11 12:57:21 +0100

    [feat]: updated exceptions handler

--- src/main/java/it/pagopa/pn/templates/engine/exceptions/ExceptionTypeEnum.java
+++ src/main/java/it/pagopa/pn/templates/engine/exceptions/ExceptionTypeEnum.java
@@ -5,8 +5,11 @@ import lombok.Getter;
 
 @Getter
 public enum ExceptionTypeEnum {
-    ERROR_FREEMARKER_BEAN_CONFIGURATION( "ERROR_FREEMARKER_BEAN_CONFIGURATION", "Errore durante la creazione del bean di configurazione FreeMarker"),
-    ERROR_TEMPLATES_DOCUMENT_COMPOSITION( "ERROR_TEMPLATES_DOCUMENT_COMPOSITION", "Errore durante la generazione del template");
+    ERROR_FREEMARKER_BEAN_CONFIGURATION( "ERROR_FREEMARKER_BEAN_CONFIGURATION", "Errore durante la creazione del bean di configurazione FreeMarker."),
+    ERROR_TEMPLATES_DOCUMENT_COMPOSITION( "ERROR_TEMPLATES_DOCUMENT_COMPOSITION", "Errore durante la generazione del template."),
+    ERROR_PDF_DOCUMENT_GENERATION( "ERROR_PDF_DOCUMENT_GENERATION", "Errore durante la generazione del pdf."),
+    ERROR_OBJECT_MAPPING( "ERROR_OBJECT_MAPPING", "Errore durante la mappatura di un oggetto."),
+    ERROR_FILE_READING( "ERROR_IMAGE_READING", "Errore durante la lettura del file.");
 
     private final String title;
     private final String message;
--- src/main/java/it/pagopa/pn/templates/engine/exceptions/RestExceptionHandler.java
+++ src/main/java/it/pagopa/pn/templates/engine/exceptions/RestExceptionHandler.java
@@ -1,9 +1,16 @@
 package it.pagopa.pn.templates.engine.exceptions;
 
 import com.fasterxml.jackson.databind.JsonMappingException;
+import it.pagopa.pn.commons.utils.MDCUtils;
+import it.pagopa.pn.templates.engine.generated.openapi.server.v1.dto.Problem;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import reactor.core.publisher.Mono;
+import java.time.Instant;
+
 
 @Slf4j
 @ControllerAdvice
@@ -14,4 +21,23 @@ public class RestExceptionHandler {
         log.error("Returning HTTP 400 Bad Request {}", e.getMessage());
     }
 
+    @ExceptionHandler(PnGenericException.class)
+    public Mono<ResponseEntity<Problem>> handleResponseEntityException(final PnGenericException exception){
+        log.warn(exception.toString());
+        final Problem problem = new Problem();
+        settingTraceId(problem);
+        problem.setTitle(exception.getExceptionType().getTitle());
+        problem.setDetail(exception.getMessage());
+        problem.setStatus(exception.getHttpStatus().value());
+        problem.setTimestamp(Instant.now());
+        return Mono.just(ResponseEntity.status(exception.getHttpStatus()).body(problem));
+    }
+
+    private void settingTraceId(Problem problem){
+        try {
+            problem.setTraceId(MDC.get(MDCUtils.MDC_TRACE_ID_KEY));
+        } catch (Exception exception) {
+            log.warn("Cannot get traceid", exception);
+        }
+    }
 }
