c96fb170dd844f04855b232175335712ec52c077
pasqualino-cristaudo-acn <pasqualino.cristaudo@accenture.com>
2024-11-11 15:08:38 +0100

    [fix]: fixed api response return

--- src/main/java/it/pagopa/pn/templates/engine/rest/TemplateApiController.java
+++ src/main/java/it/pagopa/pn/templates/engine/rest/TemplateApiController.java
@@ -1,25 +1,21 @@
 package it.pagopa.pn.templates.engine.rest;
 
-import it.pagopa.pn.templates.engine.generated.openapi.server.v1.api.PdfTemplateApi;
+import it.pagopa.pn.templates.engine.generated.openapi.server.v1.api.TemplateApi;
 import it.pagopa.pn.templates.engine.generated.openapi.server.v1.dto.*;
-import lombok.AllArgsConstructor;
 import it.pagopa.pn.templates.engine.mapper.TemplateMapper;
 import it.pagopa.pn.templates.engine.service.TemplateService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
-import java.io.ByteArrayInputStream;
-import java.nio.charset.StandardCharsets;
 
 
 @Slf4j
 @RestController
-public class TemplateApiController implements PdfTemplateApi {
+public class TemplateApiController implements TemplateApi {
     private final TemplateService templateService;
 
     public TemplateApiController(TemplateService templateService) {
@@ -69,15 +65,14 @@ public class TemplateApiController implements PdfTemplateApi {
     }
 
     @Override
-    public Mono<ResponseEntity<org.springframework.core.io.Resource>> notificationAAR(LanguageEnum xLanguage,
+    public Mono<ResponseEntity<String>> notificationAAR(LanguageEnum xLanguage,
                                                                                       Mono<NotificationAAR> request,
                                                                                       final ServerWebExchange exchange) {
         String templateName = "notificationAAR";
         return TemplateMapper.getMapByDto(request)
                 .flatMap(mappedTemplate -> templateService.genNotificationAAR(templateName, xLanguage.getValue(), Mono.just(mappedTemplate)))
                 .map(htmlText -> {
-                    ByteArrayInputStream inputStream = new ByteArrayInputStream(htmlText.getBytes(StandardCharsets.UTF_8));
-                    return ResponseEntity.accepted().body(new InputStreamResource(inputStream));
+                    return ResponseEntity.accepted().body(htmlText);
                 });
     }
 
