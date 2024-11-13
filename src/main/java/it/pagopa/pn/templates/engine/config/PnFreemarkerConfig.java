2b4269c75662f8aa55f965808f99368fc7760ef3
adrian.borta <adrian.borta@accenture.com>
2024-11-13 17:57:28 +0100

    [feat] - TemplateConfig load template as string

--- src/main/java/it/pagopa/pn/templates/engine/config/PnFreemarkerConfig.java
+++ src/main/java/it/pagopa/pn/templates/engine/config/PnFreemarkerConfig.java
@@ -10,7 +10,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
 
 import java.util.HashMap;
 
