105c6c7a79d2c114548f19c89ee575fbf047c837
Francesco Bianchi <f.bianchi@codermine.com>
2024-11-27 14:21:21 +0100

    use "mjml" as library function

--- scripts/templates-builder/src/buildMjml.js
+++ scripts/templates-builder/src/buildMjml.js
@@ -1,9 +1,8 @@
 const path = require("node:path");
 const fs = require("fs-extra");
-const util = require("node:util");
 const { BASE_SOURCE_DIR } = require("./utils");
 const log = require("./logger");
-const exec = util.promisify(require("node:child_process").exec);
+const mjmlToHtml = require("mjml");
 
 const templatesDir = path.join(BASE_SOURCE_DIR, "templates");
 
@@ -23,7 +22,9 @@ async function processMJMLFiles(dir) {
         const outputFile = path.join(dir, "index.html");
 
         log.debug(`Processing ${fullPath}`);
-        await exec(`npx mjml ${fullPath} -o ${outputFile}`);
+        const mjml = await fs.readFile(fullPath, "utf8");
+        const { html } = mjmlToHtml(mjml);
+        await fs.writeFile(outputFile, html);
       }
     }
   } catch (err) {
