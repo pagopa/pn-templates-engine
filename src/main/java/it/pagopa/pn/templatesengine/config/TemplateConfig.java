96055d93ecdae137b5680b27e7abe52d527fc6d0
Giacomo Vallorani <giacomo.vallorani@pagopa.it>
2025-03-26 17:55:52 +0100

    feat(PN-14118): update subnets

--- src/main/java/it/pagopa/pn/templatesengine/config/TemplateConfig.java
+++ src/main/java/it/pagopa/pn/templatesengine/config/TemplateConfig.java
@@ -1,11 +1,6 @@
 package it.pagopa.pn.templatesengine.config;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import it.pagopa.pn.templatesengine.exceptions.ExceptionTypeEnum;
 import it.pagopa.pn.templatesengine.exceptions.PnGenericException;
 import it.pagopa.pn.templatesengine.generated.openapi.server.v1.dto.LanguageEnum;
